b805f96c4e29734f9b734999a159f1a8667f8a49
SavonEvgeniy <88876959+SavonEvgeniy@users.noreply.github.com>
2021-09-29 21:11:19 +0500

    Update 13.8.19.py

--- 13.8.19.py
+++ 13.8.19.py
@@ -1,22 +1,22 @@
 hm_ticket=int(input('Введите количество билетов которые которые желаете приобрести:'))
-L=[input(f'Введите возраст посетителя с билетом {i+1}:')for i in range(hm_ticket)]
+list_of_age=[input(f'Введите возраст посетителя с билетом {i+1}:')for i in range(hm_ticket)]
 #обявляем стоимость билетов
-price1=0
-price2=990
-price3=1390
+price_1=0
+price_2=990
+price_3=1390
 #обьявляем списки возрастов
-age1=[i for i in range(18)]
-age2=[i for i in range(18,25)]
-age3=[i for i in range(25,100)]
-for i in range(len(L)):
-    if int(L[i]) in age1:
-          price1+=price1
-    if int(L[i]) in age2:
-          price1+=price2
-    if int(L[i]) in age3:
-          price1+=price3
+age_1=[i for i in range(18)]
+age_2=[i for i in range(18,25)]
+age_3=[i for i in range(25,100)]
+for i in range(len(list_of_age)):
+    if int(list_of_age[i]) in age_1:
+          price_1+=price_1
+    if int(list_of_age[i]) in age_2:
+          price_1+=price_2
+    if int(list_of_age[i]) in age_3:
+          price_1+=price_3
 #расчет скидки 10% если куплено более 3 билетов
-if len(L)>3:
-    print("Общаяя стоимость, с учетом скидки 10%:", price1*(0.9),"рублей")
+if len(list_of_age)>3:
+    print("Общаяя стоимость, с учетом скидки 10%:", price_1*(0.9),"рублей")
 else:
-    print("Общаяя стоимость:", price1, "рублей")
+    print("Общаяя стоимость:", price_1, "рублей")
