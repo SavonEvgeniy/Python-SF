ac66ed85983f0a9dee518c7a7bdac51974ba717c
SavonEvgeniy <88876959+SavonEvgeniy@users.noreply.github.com>
2021-09-29 21:00:27 +0500

    Update 12.7.3.py

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,8 +1,8 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = int(input("Введите сумму депозита:"))
-per_cent_values = list(per_cent.values())
-profit = [round((i*money)/100,2) for i in per_cent_values]
-max_profit = max(profit)
+per_cent_values = list(per_cent.values()) #формируем список значений из per_cent
+profit = [round((i*money)/100,2) for i in per_cent_values] #генератор списка
+max_profit = max(profit) #поиск максимального значения
 print(profit)
 print("Максимальная сумма, которую вы можете заработать-", round(max_profit,2))
 
